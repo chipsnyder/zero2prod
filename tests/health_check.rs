e592e0b2e5025a72a1555eb9001f157f40721c79
Chip <chip.snyder3@gmail.com>
2022-11-08 22:21:40 -0500

    Set up config files and begin testing Postgres record creation
    Tests fail right now but that's epected

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,9 @@
 //! tests/health_check.rs
 
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
@@ -32,6 +34,11 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed tl read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+    .await
+    .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -44,6 +51,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+    .fetch_one(&mut connection)
+    .await
+    .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
