a2ff2e40b53e052107a3f952404cb8559f2ab705
Chip <chip.snyder3@gmail.com>
2023-03-01 13:58:04 -0500

    Chapter 7 Progress

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,4 @@
 use crate::domain::SubscriberEmail;
-use crate::email_client::EmailClient;
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
@@ -57,11 +56,17 @@ impl DatabaseSettings {
 pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
+    pub authorization_token: Secret<String>,
+    pub timeout_milliseconds: u64,
 }
 
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
 
