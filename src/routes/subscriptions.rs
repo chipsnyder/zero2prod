0b795a3b56b485ac584193e59bf8fa0c26ead40a
Chip <chip.snyder3@gmail.com>
2023-07-26 11:08:35 -0400

    Chapter 9

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -146,7 +146,7 @@ pub async fn send_confirmation_email(
     );
     email_client
         .send_email(
-            new_subscriber.email,
+            &new_subscriber.email,
             "Welcome!",
             &format!(
                 "Welcome to our newsletter!<br/>\
@@ -190,7 +190,7 @@ fn generate_subscription_token() -> String {
         .collect()
 }
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
